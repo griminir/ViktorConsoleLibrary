369950b1925fd214284b455056bafd8483e012c1
griminir <viktor.elliot.king@gmail.com>
2024-06-03 10:28:58 +0200

    Add project files.

---
+++ ViktorConsoleLibrary/ConsoleHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViktorConsoleLibrary
+{
+    public static class ConsoleHelper
+    {
+        public static void PrintToConsole(this string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static string RequestString(this string message)
+        {
+            var output = "";
+
+
+            while (string.IsNullOrWhiteSpace(output))
+            {
+                Console.Write(message);
+                output = Console.ReadLine();
+            }
+
+            return output;
+        }
+
+        private static int RequestInt(this string message, bool useLowerUpper, int lower = 0, int upper = 0)
+        {
+            var output = 0;
+            var isValidAge = false;
+            var isInValidRange = true;
+
+            while (isValidAge == false || isInValidRange == false)
+            {
+                Console.Write(message);
+                isValidAge = int.TryParse(Console.ReadLine(), out output);
+
+                if (useLowerUpper == true)
+                {
+                    isInValidRange = (output >= lower && output <= upper);
+                    //if (output <= lower && output >= upper)
+                    //{
+                    //    isInValidRange = true;
+                    //}
+                    //else
+                    //{
+                    //    isInValidRange = false;
+                    //}
+                }
+            }
+
+            return output;
+        }
+
+        public static int RequestInt(this string message)
+        {
+            return message.RequestInt(false);
+        }
+
+        public static int RequestInt(this string message, int lower, int upper)
+        {
+            return message.RequestInt(true, lower, upper);
+        }
+    }
+}
